dc707b260c6d1ae270188da5746e8804c33a154a
Alexandre-Arauj <alexandrearaujo2299@gmail.com>
2022-11-24 11:30:42 -0300

    first commit

---
+++ components/Banner/index.tsx
@@ -0,0 +1,24 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
+import 'swiper/css';
+import styles from './styles.module.css'
+
+export const Banner = () => {
+    return (
+        <div className={styles.container}>
+            <Swiper
+                slidesPerView={1}
+                className={styles.swiper}
+                autoplay={{
+                    delay: 1000,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+                loop={true}
+            >
+                <SwiperSlide className={styles.slide}><div className={styles.slideImg}>1</div></SwiperSlide>
+                <SwiperSlide className={styles.slide}><div className={styles.slideImg}>2</div></SwiperSlide>
+            </Swiper>
+        </div>
+    )
+}
---
+++ libs/UseApi.ts
@@ -0,0 +1,33 @@
+export type getTenantResponse = {
+    name: string
+    mainColor: string
+    secondColor: string
+}
+
+
+export const useApi = () => ({
+
+    getTenant: (tenantSlug: string ): boolean | getTenantResponse => {
+        switch(tenantSlug){
+            case 'b7burger':
+                return {
+                    name: 'B7Burguer',
+                    mainColor: '#FF0000',
+                    secondColor: '#00FF00'
+                }
+            break
+
+            case 'b7pizza':
+                return {
+                    name: 'B7Pizza',
+                    mainColor: '#0000FF',
+                    secondColor: '#00FF00'
+                }
+            break
+            
+            default: return false
+        }
+
+    }
+
+})
